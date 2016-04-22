3d380b205bd365e82fc3c395f2b8260cb335582d
Svalorzen <svalorzen@gmail.com>
2016-04-22 23:16:37 +0200

    Fixed main problems with camera

--- scripts/RTSCameraFree.js
+++ scripts/RTSCameraFree.js
@@ -1,179 +1,87 @@
 ﻿//import System.Collections;
 
-public var fov = 60;
-public var fovmax = 90;
-public var min =30;
+// Speed of scrolling
+var scrollSpeed : float;
+var scrollEdge : float;
+// Whether we want to use the mouse to scroll
+var scrollWithMouse;
 
-var ScrollSpeed:float = 15;
-var ScrollEdge:float = 0.01;
+// Mouse rotation speed
+var rotationSpeed : float;
 
-private var HorizontalScroll:int = 1;
-private var VerticalScroll:int = 1;
-private var DiagonalScroll:int = 1;
+// Zoom scrollwheel speed
+var zoomSpeed : float;
 
-var PanSpeed:float = 10;
-
-var ZoomRange:Vector2 = Vector2(-5,5);
-var CurrentZoom:float = 0;
-var ZoomZpeed:float = 1;
-var ZoomRotation:float = 1;
-
-var rotationSpeed : float = 1.0;
-
-private var InitPos:Vector3;
-private var InitRotation:Vector3;
-
-var minCamHeight:float;
-var maxCamHeight:float;
+// Min/max camera heights
+var minCamHeight : float;
+var maxCamHeight : float;
 
 function Start() {
-    //Instantiate(Arrow, Vector3.zero, Quaternion.identity);
-    InitPos = transform.position;
-    InitRotation = transform.eulerAngles;
+    // Init camera height as 70
     Camera.main.transform.position.y = Terrain.activeTerrain.SampleHeight(GetComponent.<Camera>().transform.position)+70;
 
-    minCamHeight = 65;
-    maxCamHeight = 75;
+    scrollSpeed = 90.0;
+    scrollEdge = 0.01;
+    scrollWithMouse = 0;
+
+    rotationSpeed = 1.0;
+
+    zoomSpeed = 60.0;
+
+    minCamHeight = 65.0;
+    maxCamHeight = 300.0;
 }
 
 function Update () {
-    //PAN
-    minCamHeight = 65;
-    maxCamHeight = 75;
+    // We go faster the higher we are
+    var y = Camera.main.transform.position.y;
+    var right = 0;
+    if ( Input.GetKey("d") || (scrollWithMouse && Input.mousePosition.x >= Screen.width * (1 - scrollEdge)) )
+        right = 1;
+    else if ( Input.GetKey("a") || (scrollWithMouse && Input.mousePosition.x <= Screen.width * scrollEdge) )
+        right = -1;
 
-    if ( Input.GetKey("mouse 2") ) {
-        // (Input.mousePosition.x - Screen.width * 0.5)/(Screen.width * 0.5)
-        // transform.Translate(Vector3.right * Time.deltaTime * PanSpeed * (Input.mousePosition.x - Screen.width * 0.5)/(Screen.width * 0.5), Space.World);
-        // transform.Translate(Vector3.forward * Time.deltaTime * PanSpeed * (Input.mousePosition.y - Screen.height * 0.5)/(Screen.height * 0.5), Space.World);
-    } else {
-        if ( Input.GetKey("d") || Input.mousePosition.x >= Screen.width * (1 - ScrollEdge) ) {
-            transform.Translate(
-                Time.deltaTime * ScrollSpeed *System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                0,
-                Time.deltaTime * -ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                Space.World
-            );
-            // transform.Translate(Vector3.right * Time.deltaTime * ScrollSpeed, Space.World);
-        } else if ( Input.GetKey("a") || Input.mousePosition.x <= Screen.width * ScrollEdge ) {
-            transform.Translate(
-                Time.deltaTime * -ScrollSpeed *System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                0,
-                Time.deltaTime * ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                Space.World
-            );
-            // transform.Translate(Vector3.right * Time.deltaTime * -ScrollSpeed, Space.World);
-        }
-
-        if ( Input.GetKey("w") || Input.mousePosition.y >= Screen.height * (1 - ScrollEdge) ) {
-            transform.Translate(
-                Time.deltaTime * ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                0,
-                Time.deltaTime * ScrollSpeed *System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                Space.World
-            );
-            // transform.Translate(Vector3.forward * Time.deltaTime * ScrollSpeed *System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180), Space.World);
-        } else if ( Input.GetKey("s") || Input.mousePosition.y <= Screen.height * ScrollEdge ) {
-            transform.Translate(
-                Time.deltaTime * -ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                0,
-                Time.deltaTime * -ScrollSpeed *System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                Space.World
-            );
-            // transform.Translate(Vector3.forward * Time.deltaTime * -ScrollSpeed*System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180), Space.World);
-            // transform.Translate(Vector3.forward * Time.deltaTime * -ScrollSpeed*System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180), Space.World);
-        }
-    }
-
-    // var fwd = transform.TransformDirection (Vector3.forward);
-    //  if (Physics.Raycast (transform.position, fwd, 10)) {
-    //      print (fwd);
-    //  }
-
-    //ZOOM IN/OUT
-    var fwd = transform.TransformDirection (Vector3.forward);
-    if (Input.GetAxis("Mouse ScrollWheel")> 0) {
+    if ( right ) {
         transform.Translate(
-            2*Time.deltaTime * ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-            2*Time.deltaTime * -ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.x)*System.Math.PI/180)*System.Math.Pow(2.0,0.5),
-            2*Time.deltaTime * ScrollSpeed *System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
+            Time.deltaTime * right * (scrollSpeed + (y - minCamHeight)/2) * System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
+            0,
+            Time.deltaTime * -right * (scrollSpeed + (y - minCamHeight)/2) *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
             Space.World
         );
-    //  if((Terrain.activeTerrain.SampleHeight(camera.transform.position)+70)<40){
-    //      transform.Translate(
-    //      2*Time.deltaTime * -ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-    //      2*Time.deltaTime * ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.x)*System.Math.PI/180)*System.Math.Pow(2.0,0.5),
-    //      2*Time.deltaTime * -ScrollSpeed *System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-    //      Space.World
-    //  );
-    //  }
-        if(Camera.main.transform.rotation.eulerAngles.x <= 180){
-            if (Physics.Raycast (transform.position, fwd, 50)) {
-                    transform.Translate(
-                    2*Time.deltaTime * -ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                    2*Time.deltaTime * ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.x)*System.Math.PI/180)*System.Math.Pow(2.0,0.5),
-                    2*Time.deltaTime * -ScrollSpeed *System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                    Space.World
-                );
-            }
-        }
-        if(Camera.main.transform.rotation.eulerAngles.x >= 180){
-            if (!(Physics.Raycast (transform.position, -fwd, 150))) {
-                transform.Translate(
-                    2*Time.deltaTime * -ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                    2*Time.deltaTime * ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.x)*System.Math.PI/180)*System.Math.Pow(2.0,0.5),
-                    2*Time.deltaTime * -ScrollSpeed *System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                    Space.World
-                );
-            }
-        }
-        // Camera.main.fieldOfView = fov--;
     }
-    if (Input.GetAxis("Mouse ScrollWheel")< 0) {
+
+    var forward = 0;
+    if ( Input.GetKey("w") || (scrollWithMouse && Input.mousePosition.y >= Screen.height * (1 - scrollEdge)) )
+        forward = 1;
+    else if ( Input.GetKey("s") || (scrollWithMouse && Input.mousePosition.y <= Screen.height * scrollEdge) )
+        forward = -1;
+
+    if ( forward ) {
         transform.Translate(
-            2*Time.deltaTime * -ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-            2*Time.deltaTime * ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.x)*System.Math.PI/180)*System.Math.Pow(2.0,0.5),
-            2*Time.deltaTime * -ScrollSpeed *System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
+            Time.deltaTime * forward * (scrollSpeed + (y - minCamHeight)/2) * System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y) * System.Math.PI/180),
+            0,
+            Time.deltaTime * forward * (scrollSpeed + (y - minCamHeight)/2) * System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y) * System.Math.PI/180),
             Space.World
-            //0.2*System.Math.Pow((Terrain.activeTerrain.SampleHeight(camera.transform.position))/(System.Math.Cos((90-Camera.main.transform.rotation.eulerAngles.x)*System.Math.PI/180)),2)
         );
-//      var fwd = transform.TransformDirection (Vector3.forward);
-        if(Camera.main.transform.rotation.eulerAngles.x >= 180){
-            if (Physics.Raycast (transform.position, -fwd, 50)) {
-                transform.Translate(
-                    2*Time.deltaTime * ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                    2*Time.deltaTime * -ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.x)*System.Math.PI/180)*System.Math.Pow(2.0,0.5),
-                    2*Time.deltaTime * ScrollSpeed *System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                    Space.World
-                );
-            }
-        }
-        if(Camera.main.transform.rotation.eulerAngles.x <= 180){
-            if (!(Physics.Raycast (transform.position, fwd, 150))) {
-                transform.Translate(
-                    2*Time.deltaTime * ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                    2*Time.deltaTime * -ScrollSpeed *System.Math.Sin((Camera.main.transform.rotation.eulerAngles.x)*System.Math.PI/180)*System.Math.Pow(2.0,0.5),
-                    2*Time.deltaTime * ScrollSpeed *System.Math.Cos((Camera.main.transform.rotation.eulerAngles.y)*System.Math.PI/180),
-                    Space.World
-                );
-            }
-        }
-    // Camera.main.fieldOfView = fov++;
     }
+
+    // Zoom in(>0) and out (<0)
+    var zoom = Input.GetAxis("Mouse ScrollWheel");
+    zoom = zoom ? ( zoom < 0 ? -1 : 1 ) : 0;
+    // Only zoom if we're not on min level
+    if ( zoom && (( zoom > 0 && minCamHeight < y ) || ( zoom < 0 && y < maxCamHeight )) )
+        transform.Translate((zoom * zoomSpeed) * Vector3.forward);
 
     // ROTATION
     if (Input.GetMouseButton(1)) {
-        var h : float = rotationSpeed * Input.GetAxis ("Mouse X");
-        var v : float = rotationSpeed * Input.GetAxis ("Mouse Y");
-        transform.Rotate (0, h, 0, Space.World);
+        var h : float = -rotationSpeed * Input.GetAxis("Mouse X");
+        var v : float = rotationSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(0, h, 0, Space.World);
+        transform.Rotate(v, 0, 0);
 
-        transform.Rotate (v, 0, 0);
-
-        if((Camera.main.transform.rotation.eulerAngles.x >= 90) &&(Camera.main.transform.rotation.eulerAngles.x <= 180))
-            transform.Rotate (-v, 0, 0);
-        if(((Camera.main.transform.rotation.eulerAngles.x >= 180)&&(Camera.main.transform.rotation.eulerAngles.x <= 270))||(Camera.main.transform.rotation.eulerAngles.x < 0) )
-            transform.Rotate (-v, 0, 0);
-        if((Camera.main.transform.rotation.eulerAngles.z >= 160)&&(Camera.main.transform.rotation.eulerAngles.z <= 200))
-            transform.Rotate (-v, 0, 0);
+        var x = Camera.main.transform.rotation.eulerAngles.x;
+        if(x < 0 || x > 80)
+             transform.Rotate(-v, 0, 0);
     }
     // Following terraint (optional)
     // if(Terrain.activeTerrain.SampleHeight(camera.transform.position)+70<minCamHeight)
@@ -181,8 +89,3 @@ function Update () {
     // if(Terrain.activeTerrain.SampleHeight(camera.transform.position)+70>maxCamHeight)
     //     Camera.main.transform.position.y = maxCamHeight;
 }
-
-// var horiz : float = Input.GetAxis("Horizontal");
-// transform.Translate(Vector3(horiz,0,0));
-// var vert : float = Input.GetAxis("Vertical");
-// transform.Translate(Vector3(0,0,vert));
